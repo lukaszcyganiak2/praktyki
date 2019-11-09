15b96b930397340ed992315aa1d2b9256b4e66bb
lukaszcyganiak <lukaszcyganiak@interia.pl>
2019-11-09 14:28:13 +0100

    praktyki  add sth

--- app/main.js
+++ app/main.js
@@ -1,14 +1,36 @@
 function getBooks() {
     const booksId = document.getElementById('books')
-    const rowDiv = document.createElement('div')
-    rowDiv.classList.add('row')
-    console.log("TCL: getBooks -> rowDiv", rowDiv)
+    booksId.innerHTML = ''
+
 
     fetch('http://localhost:3000/books')
         .then(books => books.json())
         .then(books => {
-            books.forEach((element, index) => {
-                console.log(element.title, "index", index)
+            books.forEach(element => {
+                const card = createdEl('div', 'class', 'card main-card', '')
+                const cardBody = createdEl('div', 'class', 'card-body', '')
+                const cardH5 = createdEl('h5', 'class', 'card-title', element.title)
+                const cardH6 = createdEl('h6', 'class', 'card-subtitle mb-2 text-muted', element.author)
+                const cardParagraf = createdEl('p', 'class', 'card-text', element.description)
+                const buttonEdit = createdEl('button', 'class', 'btn btn-secondary main-btn', "Edytuj")
+                buttonEdit.onclick = function () {
+                    editeBook(element)
+                }
+                const buttonDelete = createdEl('button', 'class', 'btn btn-danger main-btn', "Usuń")
+                buttonDelete.onclick = function () {
+                    deleteBook(element)
+                }
+                const cardFooter = createdEl('div', 'class', 'card-footer', "")
+                cardFooter.appendChild(buttonEdit)
+                cardFooter.appendChild(buttonDelete)
+                cardBody.appendChild(cardH5)
+                cardBody.appendChild(cardH6)
+                cardBody.appendChild(cardParagraf)
+                card.appendChild(cardBody)
+                card.appendChild(cardFooter)
+                booksId.appendChild(card)
+
+
             });
         })
 
@@ -16,3 +38,26 @@ function getBooks() {
 
 
 getBooks()
+
+/**
+ * 
+ * @param {String} el // el to jest TAg HTML który chcemy stworzyć np. div lub span itp
+ * @param {String} attribute  // attribute to atrybut naszego TAGa HTML np. class, id, href itp
+ * @param {String} attributeValue // attributeValue to wartości naszego atrubutu np nazwa class
+ * @param {String} text // text czyli tekst który wyświetli się w stworzonym atrybucie
+ */
+function createdEl(el, attribute, attributeValue, text) {
+    const element = document.createElement(el)
+    element.setAttribute(attribute, attributeValue)
+    element.innerHTML = text
+    return element
+}
+
+
+function editeBook(book) {
+    console.log(book)
+}
+
+function deleteBook(book) {
+    console.log(book)
+}
